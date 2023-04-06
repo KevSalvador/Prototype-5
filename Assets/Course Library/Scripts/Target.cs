53c58e27816ab5a09f5b36d907ac1265a05c92b4
Kevin <Kesalvador@s.sfusd.edu>
2023-04-06 11:24:29 -0700

    5.2 step 3 complete

--- Assets/Course Library/Scripts/Target.cs
+++ Assets/Course Library/Scripts/Target.cs
@@ -36,7 +36,7 @@ public class Target : MonoBehaviour
     
     private void OnTriggerEnter(Collider other)
     {
-      Destroy(gameObject);
+      Destroy(gameObject); 
     }
     
     Vector3 RandomForce()
