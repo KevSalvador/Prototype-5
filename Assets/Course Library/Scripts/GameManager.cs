b8247ec060d6e6c69b55c80d10b5a84a15b3aa8f
Kevin <Kesalvador@s.sfusd.edu>
2023-04-12 10:24:23 -0700

    5.3 step 5 stop spawning when Game Over

--- Assets/Course Library/Scripts/GameManager.cs
+++ Assets/Course Library/Scripts/GameManager.cs
@@ -9,17 +9,18 @@ public class GameManager : MonoBehaviour
     public List<GameObject> targets;
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI gameOverText;
+    public bool isGameActive; 
     private int score; 
     private float spawnRate = 1.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(SpawnTarget());
+        isGameActive= true;
         score = 0;
-        UpdateScore(0);
-
        
+         StartCoroutine(SpawnTarget());
+        UpdateScore(0);
     }
 
     // Update is called once per frame
@@ -29,7 +30,7 @@ public class GameManager : MonoBehaviour
     }
     IEnumerator SpawnTarget()
     {
-        while (true)
+        while (isGameActive)
         {
            yield return new WaitForSeconds(spawnRate);
           int index = Random.Range(0, targets.Count);
@@ -46,6 +47,7 @@ public class GameManager : MonoBehaviour
    public void GameOver()
    {
     gameOverText.gameObject.SetActive(true);
+    isGameActive = false; 
    }
 }
 
