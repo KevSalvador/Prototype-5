2b73af435b5bc44daedafe0821ccd77c74dd6042
Kevin <Kesalvador@s.sfusd.edu>
2023-04-14 15:11:28 -0700

    5.4 step 3,4&5 complete

--- Assets/Course Library/Scripts/DifficultyButton.cs
+++ Assets/Course Library/Scripts/DifficultyButton.cs
@@ -1,13 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DifficultyButton : MonoBehaviour
 {
+    private Button button;
+    private GameManager gameManager; 
+
+    public int difficulty; 
     // Start is called before the first frame update
     void Start()
     {
-        
+        button = GetComponent<Button>();
+         gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+
+        button.onClick.AddListener(SetDifficulty); 
     }
 
     // Update is called once per frame
@@ -15,4 +23,12 @@ public class DifficultyButton : MonoBehaviour
     {
         
     }
+     void SetDifficulty()
+     {
+        Debug.Log(button.gameObject.name + "was clicked");
+        gameManager.StartGame(); 
+     }
+
+
+
 }
